2f74d9de698d6859b9a341e3ce02ce8aa0e34c3d
chopafor <jgallo@cashstar.com>
2016-12-12 11:50:15 -0500

    encode unicode strings to UTF-8 so that python can urlencode them

--- nap/utils.py
+++ nap/utils.py
@@ -1,21 +1,7 @@
 from __future__ import unicode_literals
 import itertools
 from six.moves.urllib.parse import urlencode
-from six.moves.urllib.parse import parse_qsl, urlsplit, urlunsplit
 import six
-from six.moves import urllib
-
-
-def normalize_url(url_string):
-    url = urlsplit(url_string)
-    if url.query:
-        query_params = sorted(parse_qsl(url.query, keep_blank_values=True), key=lambda key_value: key_value[0])
-        query_normalized = urlencode(query_params)
-        url = url._replace(query=query_normalized)
-
-    # urlsplit'ing and urlunsplit'ing does some normalization, so apply them even if there is not a query string.
-    # See https://docs.python.org/2/library/urlparse.html#urlparse.urlunsplit for more details.
-    return urlunsplit(url)
 
 
 def handle_slash(url, add_slash=None):
@@ -46,12 +32,20 @@ def make_url(base_url, params=None, add_slash=None):
 
     if params:
 
+        def safe_encode(value):
+            try:
+                if isinstance(value, unicode):
+                    return value.encode('utf-8')
+                return value
+            except AttributeError:
+                return value
+
         # If we're given an non-string iterable as a params value,
         # we want to pass in multiple instances of that param key.
         def flatten_params(k, vs):
             if not hasattr(vs, '__iter__') or is_string_like(vs):
-                return ((k, vs),)
-            return [(k, v) for v in vs]
+                return ((k, safe_encode(vs)),)
+            return [(k, safe_encode(v)) for v in vs]
 
         flat_params = [
             flatten_params(k, v)
@@ -60,19 +54,21 @@ def make_url(base_url, params=None, add_slash=None):
 
         # since we can have more than one value for a single key, we use a
         # tuple of two tuples instead of a dictionary
-        params_tuple = tuple(itertools.chain(*flat_params))
-        param_string = urllib.parse.urlencode(params_tuple)
+        params_tuple = tuple(sorted(itertools.chain(*flat_params)))
+        param_string = urlencode(params_tuple)
         base_url = "%s?%s" % (base_url, param_string)
 
     return base_url
 
 
 __text_fn = str if six.PY3 else unicode
-def to_unicode(s): 
+
+
+def to_unicode(s):
     if s is None:
         return None
 
-    # unicode strings    
+    # unicode strings
     elif isinstance(s, six.text_type):
         return s
 
