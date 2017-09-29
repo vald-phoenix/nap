6bf843e9f13fae0f4d17ae388a7c0b8b2f233752
Matt Davis <matteius@gmail.com>
2017-09-29 17:11:02 -0400

    Add concept of hashing a cache key above a specified length
    This allows us to work around memcached limits while keeping, mostly,
    plaintext keys in memcached.

--- nap/cache/base.py
+++ nap/cache/base.py
@@ -1,16 +1,22 @@
 from __future__ import unicode_literals
 import re
+from hashlib import md5
 
 DEFAULT_TIMEOUT = 60 * 5
+# Default value for max cache key length favors memcached limitation of 250 byte key
+# and the assumption the web framework may append additional version to the key.
+MAX_CACHE_KEY_LENGTH = 240
 
 
 class BaseCacheBackend(object):
 
     CACHE_EMPTY = "!!!DNE!!!"
 
-    def __init__(self, default_timeout=DEFAULT_TIMEOUT, obey_cache_headers=True):
+    def __init__(self, default_timeout=DEFAULT_TIMEOUT,
+                 obey_cache_headers=True, cache_max_key_size=MAX_CACHE_KEY_LENGTH):
         self.obey_cache_headers = obey_cache_headers
         self.default_timeout = default_timeout
+        self.cache_max_key_size = cache_max_key_size
 
     def get(self, key):
         return None
@@ -28,16 +34,19 @@ class BaseCacheBackend(object):
             return int(cache_header_age.group(1))
 
     def get_cache_key(self, model, url):
-
+        """ Cache key format is %(resource_name)s::%(url)s where the URL may be
+          md5 hashed when it exceeds length.  strips white space from cache key as
+           its a control character in memcached.
+        """
         resource_name = model._meta['resource_name']
+        cache_key = "{0}::".format(resource_name)
 
-        key_parts = {
-            'resource_name': resource_name,
-            'url': url,
-        }
+        if (len(cache_key) + len(url)) > self.cache_max_key_size:
+            cache_key += md5(url.encode('utf-8')).hexdigest()
+        else:
+            cache_key += url
 
-        cache_key = "%(resource_name)s::%(url)s" % key_parts
-
+        cache_key = cache_key.replace(" ", "")
         return cache_key
 
     def get_timeout(self, response=None):
