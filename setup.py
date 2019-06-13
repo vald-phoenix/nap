b3dcc4ad629b7842ba8c9a73cfcf264449f7e6a6
Vladyslav Krylasov <vladyslav.krylasov@gmail.com>
2019-06-13 13:44:40 +0300

    🔧 ARCH-3394 Drop installation of six & mock, put a Python 3 label.

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
-from __future__ import unicode_literals
-from datetime import datetime
-from setuptools import setup, find_packages
 import io
+from datetime import datetime
 
-version = '%s' % datetime.now().strftime("%Y-%m-%d-%H_%M_%S_%f")
+from setuptools import find_packages, setup
+
+version = '{}'.format(datetime.now().strftime('%Y-%m-%d-%H_%M_%S_%f'))
 setup(
     name='nap',
     version=version,
-    description=('api access modeling and tools'),
-    author="Jacob Burch",
-    author_email="jacobburch@gmail.com",
+    description='api access modeling and tools',
+    author='Jacob Burch',
+    author_email='jacobburch@gmail.com',
     url='https://github.com/jacobb/nap',
     long_description=io.open('README.rst', encoding='utf-8').read(),
     packages=find_packages(),
@@ -19,19 +19,18 @@ setup(
     tests_require=[
         'pytest',
         'pytest-cov',
-        'mock',
         'Django>=1.8.18',
         'Flask>=0.11.1',
         'Flask-Caching>=1.2.0'
     ],
     install_requires=[
-        'requests>=1.2.3', 'six'
+        'requests>=1.2.3'
     ],
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Topic :: Utilities',
     ],
 )
